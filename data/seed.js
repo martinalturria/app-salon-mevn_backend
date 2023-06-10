b7a108de7d4e8cd6c7adf684e184401f9e6057de
Martin Alturria <martinalturria@hotmail.com>
2023-06-10 13:48:17 -0300

    add data/seed.js and route controller getServices The routes archives were modified and accommodated

---
+++ data/seed.js
@@ -0,0 +1,37 @@
+import colors from "colors";
+import dotenv from "dotenv";
+import { db } from "../config/db.js";
+import Services from "../models/Services.js";
+import { services } from "./beautyServices.js";
+
+dotenv.config();
+await db();
+
+async function seedDB() {
+    try {
+        await Services.insertMany(services);
+        console.log(colors.green.bold("Se agregaron los datos correctamente"));
+        process.exit();
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+}
+
+async function clearDB() {
+    try {
+        await Services.deleteMany();
+        console.log(colors.red.bold("Se eliminaron los datos correctamente"));
+        process.exit();
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+}
+
+
+if (process.argv[2] === "--import") {
+    seedDB();
+} else {
+    clearDB();
+}
